54f23a3b417c9460059971ee88c0d2d91c2dbc90
gitegofaycal-create <gitegofaycal@gmail.com>
2025-12-09 13:12:23 +0200

    pull updated version of home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,22 +1,19 @@
 import React from 'react'
-<<<<<<<<< Temporary merge branch 1
-import Navbar from '../components/Navbar';
-import ServiceSection from '../components/ServiseSection';
-=========
+import Testimonial from '../components/Testimonial';
 import Navbar from '../components/Navbar'
 import ServiceSection from '../components/ServiseSection'
 
->>>>>>>>> Temporary merge branch 2
+
 const Home = () => {
     return (
         <div>
             <Navbar />
-<<<<<<<<< Temporary merge branch 1
+
             <ServiceSection/>
   
-=========
+
         <Testimonial />
->>>>>>>>> Temporary merge branch 2
+
         </div>
     )
 }
